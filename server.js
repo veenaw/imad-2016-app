29981ac368047b4f5e8c6d16867375fada0e5298
veenaw <vwagle2@gmail.com>
2016-09-27 21:53:53 +0530

    Adding P4 changes

--- server.js
+++ server.js
@@ -1,19 +1,31 @@
-var express = require('express');
-var morgan = require('morgan');
+var express = require('express');// create web server
+var morgan = require('morgan'); // to help output logs of server
 var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/', function (req, res) {
+app.get('/', function (req, res) { // exectutes when ge request is made to '/'
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
+app.get('/article-one', function(req,res)){
+res.send('Article one requested and will be served here');
+}
+
+app.get('/article-two', function(req,res)){
+res.send('Article two requested and will be served here');
+}
+
+app.get('/article-three', function(req,res)){
+res.send('Article three requested and will be served here');
+}
+
+app.get('/ui/style.css', function (req, res) {  // similar as 8
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('/ui/madi.png', function (req, res) {  // similar to 8
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
