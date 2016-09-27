60bebb6f597f9d74a8882c874ec82e6799a81967
Veena Wagle <vwagle2@gmail.com>
2016-09-27 23:30:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,14 +90,6 @@ app.get('/:articleName',function(req,res){
   res.sendFile(createTemplate(articles[articleName]));
 });
 
-app.get('/article-two', function(req,res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three', function(req,res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
-
 app.get('/ui/style.css', function (req, res) {  // similar as 8
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
