5e0e47d716e16129c2b8c1f581797af91c298ef7
Veena Wagle <vwagle2@gmail.com>
2016-09-28 21:41:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,10 @@
 console.log('Loaded!');
+
+var element = document.getElementById('main-text');
+
+element.innerHTML = 'New Value';
+
+var img= document.getElementById('img');
+img.onclick = function(){
+    img.style.marginLeft= '100px';
+}
