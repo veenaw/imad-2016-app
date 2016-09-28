d229cf4fe5136cd710b8c165cc83644e72daf3ae
Veena Wagle <vwagle2@gmail.com>
2016-09-28 22:13:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,14 @@ var element = document.getElementById('main-text');
 
 element.innerHTML = 'New Value';
 
-var Img= document.getElementById('madi');
-Img.onclick = function(){
-    Img.style.marginLeft= '100px';
+function moveRight(){
+marginLeft+=10;
+img.style.marginLeft= marginLeft+'px';
+    
+}
+
+var img= document.getElementById('madi');
+
+img.onclick = function(){
+    var  inteval =setInterval(moveRight,100);
 };
